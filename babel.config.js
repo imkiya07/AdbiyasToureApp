1bf72cad9923e9421507d3d2801aba327774ec81
malinjr07 <malinjr07@gmail.com>
2024-09-16 13:35:52 +0600

    Project Setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
