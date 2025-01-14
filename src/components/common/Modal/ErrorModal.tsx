d0ba205248635f9a1e19475056b149d704ad2093
marufc9 <Mosaddik.AliMaruf@c9.com.au>
2025-01-15 01:16:14 +0600

    Error Modal Created

--- src/components/common/Modal/ErrorModal.tsx
+++ src/components/common/Modal/ErrorModal.tsx
@@ -1,14 +1,54 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {FC} from 'react';
+import {tModalProp} from '@utils/types';
 
-const ErrorModal: FC = () => {
+const ErrorModal: FC<tModalProp> = ({visible, closeModal}) => {
   return (
-    <View>
-      <Text>ErrorModal</Text>
-    </View>
+    <Modal
+      animationType="slide"
+      transparent={false}
+      visible={visible}
+      onRequestClose={closeModal}>
+      <Text style={styles.title}>Sorry!</Text>
+      <View style={styles.line} />
+      <Text style={styles.title}>
+        It Seems there is a trouble while completing your request.
+      </Text>
+      <Text style={styles.title}>Please Try Again later.</Text>
+      <Text style={styles.title}>
+        If the issue persist, please reach out to the support team with
+        screenshot of this screen!
+      </Text>
+      <Text style={styles.title}>Error Code:</Text>
+      <TouchableOpacity onPress={closeModal}>
+        <Text>Okay</Text>
+      </TouchableOpacity>
+    </Modal>
   );
 };
 
 export default ErrorModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  line: {
+    height: 1,
+    backgroundColor: '#333',
+    marginBottom: 20,
+    width: '100%',
+  },
+  pickerBtn: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#333',
+  },
+  countryName: {
+    textAlign: 'center',
+    textTransform: 'capitalize',
+  },
+});
